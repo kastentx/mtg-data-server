6ec3639e8e75d7e8d4e8462a3c27a2240e9916ae
Nick Kasten <kastencode@gmail.com>
2025-03-07 02:20:54 -0600

    set metadata endpoint

--- src/routes/api-v1.ts
+++ src/routes/api-v1.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { checkRemoteFileModified } from '../helpers/mtgJsonHelpers';
 import CardDataStore from '../store/cardData';
+import { SetList } from '../types';
 
 const router = express.Router();
 
@@ -37,5 +38,54 @@ router.get('/data', asyncHandler(async (_req, res) => {
     }
     res.json({ keys: Object.keys(data) });
 }));
+
+router.get('/sets', asyncHandler(async (_req, res) => {
+    const store = CardDataStore.getInstance();
+    const { data } = store.getData();
+    if (!data) {
+        res.status(404).json({ error: 'Card data not found' });
+        return;
+    }
+    // each key in the data object is a setList object
+    // lets return a list of setlist objects that match the setlist type 1:1
+    // const setLists = Object.keys(data).map((key) => data[key] as SetList);
+    const setLists = Object.keys(data).map((key) => {
+        const setList = data[key] as SetList;
+        return {
+            code: setList.code,
+            name: setList.name,
+            type: setList.type,
+            releaseDate: setList.releaseDate,
+            block: setList.block,
+            onlineOnly: setList.isOnlineOnly,
+        };
+    });
+    
+    res.json(setLists); 
+}));
+
+router.get('/sets/metadata', asyncHandler(async (_req, res) => {
+    const store = CardDataStore.getInstance();
+    const { data } = store.getData();
+    if (!data) {
+        res.status(404).json({ error: 'Card data not found' });
+        return;
+    }
+    
+    const allSets = Object.keys(data).map((key) => {
+        const setMetadata = data[key] as SetList;
+        return {
+            code: setMetadata.code,
+            name: setMetadata.name,
+            type: setMetadata.type,
+            releaseDate: setMetadata.releaseDate,
+            block: setMetadata.block,
+            isOnlineOnly: setMetadata.isOnlineOnly
+        };
+    });
+    
+    res.json(allSets); 
+}));
+
 
 export default router;
